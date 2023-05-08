02ebf11825f6223516ce52e35474332b4106112e
jmibarlucea <jmibarlucea@gmail.com>
2023-05-08 17:46:13 -0300

    Revert "[INT-7625] Fixed repoName not being parsed correctly"

--- src/steps/projects/codeRepo.ts
+++ src/steps/projects/codeRepo.ts
@@ -96,20 +96,15 @@ function parseSnykProjectName(projectName: string): ParseSnykProjectNameResult {
   // `projectName` can be in two different formats:
   // Input 1. `starbase-test/starbase:subdir/package.json`
   // Input 2. `starbase-test/starbase:package.json`
-  // Input 3. `starbase-test/subdir/starbase:package.json`
-
+  //
   // Output 1. ['starbase-test/starbase', 'subdir/package.json']
   // Output 2. ['starbase-test/starbase', 'package.json']
-  // Output 3. ['starbase-test/subdir/starbase', 'package.json']
-
   const [repoFullName, fileScannedPath] = projectName.split(':');
+  if (!repoFullName) return getUnknownSnykProjectNameParseResult();
 
-  if (!repoFullName || repoFullName === '')
-    return getUnknownSnykProjectNameParseResult();
-
-  // `starbase-test/subdir/starbase` => `starbase-test`
-  const [repoOrganization] = repoFullName.split('/');
-  if (!repoOrganization || repoOrganization === '')
+  // `starbase-test/starbase` => `starbase`
+  const [repoOrganization, repoName] = repoFullName.split('/');
+  if (!repoOrganization || !repoName)
     return getUnknownSnykProjectNameParseResult();
 
   let fullDirectoryPath: string | undefined;
@@ -127,7 +122,7 @@ function parseSnykProjectName(projectName: string): ParseSnykProjectNameResult {
   return {
     repoFullName: repoFullName.toLowerCase(),
     repoOrganization: repoOrganization.toLowerCase(),
-    repoName: repoFullName.toLowerCase(),
+    repoName: repoName.toLowerCase(),
     fullDirectoryPath: fullDirectoryPath?.toLowerCase(),
     topLevelDirectoryName: topLevelDirectoryName?.toLowerCase(),
     fileName,
